eb83d9a8fdb02d7585f73836515ce5a0c951950e
Tony Migeon <tonymigeonpro@gmail.com>
2024-01-12 11:58:33 +0100

    V1 of stats display

--- src/components/Filter/StatsFilter/StatsFilter.jsx
+++ src/components/Filter/StatsFilter/StatsFilter.jsx
@@ -2,40 +2,48 @@ import React, { useState } from "react";
 import cssModule from "./StatsFilter.module.scss";
 import Image from "next/image";
 
+// maitrisElementRandom should show if maitriseMelee or maitriseDistance is selected
+
 const TypeFilter = () => {
     const [selectedImages, setSelectedImages] = useState([]);
     const [selectedItems, setSelectedItems] = useState([]);
+    const [selectedCategory, setSelectedCategory] = useState(1);
 
-    const stats = [
-        "air",
-        "armure",
-        "controle",
-        "critique",
-        "eau",
-        "esquive",
-        "feu",
-        "initiative",
-        "maitriseElementRandom",
-        "maitriseMelee",
-        "maitriseSoin",
-        "malusResdos",
+    const primary_stat = [
         "pa",
         "pm",
-        "portee",
-        "prospection",
-        "pv",
-        "resAir",
+        "wakfu",
+        "portee", // end of row1
+        "maitriseBerserk",
+        "maitriseMelee",
+        "maitriseDistance",
+        "maitriseElementRandom", // end of row2
+        "controle",
+        "maitriseDos",
+        "critique",
+        "maitriseCritique", // end of row3
+    ];
+    const secondary_stat = [
+        "maitriseSoin",
+        "volonte",
+        "tacle",
+        "esquive", // end of row1
+        "initiative",
         "resCritique",
+        "hp",
+        "armure", // end of row2
+        "resAir",
         "resEau",
         "resFeu",
+        "resTerre", // end of row3
+    ];
+
+    const remaining_stats = [
+        "malusResdos",
+        "prospection",
         "ressourceRecolte",
-        "resTerre",
         "sagesse",
-        "tacle",
-        "terre",
         "up",
-        "volonte",
-        "wakfu",
     ];
 
     const handleImageClick = (imageName) => {
@@ -50,9 +58,96 @@ const TypeFilter = () => {
 
     return (
         <div className={cssModule["type-container"]}>
-            <h3 className={cssModule["type-title"]}>Stats</h3>
+            {/* <h3 className={cssModule["type-title"]}>Stats</h3> */}
             <div className={cssModule["horizontal-separator"]}></div>
             <div
+                className={`${cssModule["type-row-icon-container"]} ${cssModule["top-row"]}`}
+                onClick={() => setSelectedCategory(1)}
+            >
+                <span  className={cssModule["stat-title"]}> Stats Principales</span>
+                <div className={cssModule["global-icon-container"]}>
+                    {selectedCategory === 1 &&
+                        primary_stat.map((itemName) => (
+                            <div
+                                key={itemName}
+                                className={`${cssModule["icon-container"]} ${
+                                    selectedItems.includes(itemName)
+                                        ? cssModule["selected"]
+                                        : ""
+                                }`}
+                                onClick={() => handleImageClick(itemName)}
+                            >
+                                <Image
+                                    className={cssModule["icon"]}
+                                    src={`/stats/${itemName}.png`}
+                                    width={24}
+                                    height={24}
+                                    unoptimized
+                                    alt={itemName}
+                                />
+                            </div>
+                        ))}
+                </div>
+            </div>
+            <div
+                className={cssModule["type-row-icon-container"]}
+                onClick={() => setSelectedCategory(2)}
+            >
+                <span  className={cssModule["stat-title"]}>Stats Secondaires</span>
+                <div className={cssModule["global-icon-container"]}>
+                    {selectedCategory === 2 &&
+                        secondary_stat.map((itemName) => (
+                            <div
+                                key={itemName}
+                                className={`${cssModule["icon-container"]} ${
+                                    selectedItems.includes(itemName)
+                                        ? cssModule["selected"]
+                                        : ""
+                                }`}
+                                onClick={() => handleImageClick(itemName)}
+                            >
+                                <Image
+                                    className={cssModule["icon"]}
+                                    src={`/stats/${itemName}.png`}
+                                    width={24}
+                                    height={24}
+                                    unoptimized
+                                    alt={itemName}
+                                />
+                            </div>
+                        ))}
+                </div>
+            </div>
+            <div
+                className={cssModule["type-row-icon-container"]}
+                onClick={() => setSelectedCategory(3)}
+            >
+                <span className={cssModule["stat-title"]}>Stats Restantes</span>
+                <div className={cssModule["global-icon-container"]}>
+                    {selectedCategory === 3 &&
+                        remaining_stats.map((itemName) => (
+                            <div
+                                key={itemName}
+                                className={`${cssModule["icon-container"]} ${
+                                    selectedItems.includes(itemName)
+                                        ? cssModule["selected"]
+                                        : ""
+                                }`}
+                                onClick={() => handleImageClick(itemName)}
+                            >
+                                <Image
+                                    className={cssModule["icon"]}
+                                    src={`/stats/${itemName}.png`}
+                                    width={24}
+                                    height={24}
+                                    unoptimized
+                                    alt={itemName}
+                                />
+                            </div>
+                        ))}
+                </div>
+            </div>
+            {/* <div
                 className={`${cssModule["type-row-icon-container"]} ${cssModule["top-row"]}`}
             >
                 {stats.map((itemName) => (
@@ -72,50 +167,6 @@ const TypeFilter = () => {
                             height={24}
                             unoptimized
                             alt={itemName}
-                        />
-                    </div>
-                ))}
-            </div>
-            {/* <div className={cssModule["type-row-icon-container"]}>
-                {["132", "103", "133", "119"].map((imageName) => (
-                    <div
-                        key={imageName}
-                        className={`${cssModule["icon-container"]} ${
-                            selectedImages.includes(imageName)
-                                ? cssModule["selected"]
-                                : ""
-                        }`}
-                        onClick={() => handleImageClick(imageName)}
-                    >
-                        <Image
-                            className={cssModule["icon"]}
-                            src={`/itemTypes/${imageName}.png`}
-                            width={28}
-                            height={28}
-                            unoptimized
-                            alt="casque/helmet"
-                        />
-                    </div>
-                ))}
-            </div>
-            <div className={cssModule["type-row-icon-container"]}>
-                {["518", "519", "571", "520"].map((imageName) => (
-                    <div
-                        key={imageName}
-                        className={`${cssModule["icon-container"]} ${
-                            selectedImages.includes(imageName)
-                                ? cssModule["selected"]
-                                : ""
-                        }`}
-                        onClick={() => handleImageClick(imageName)}
-                    >
-                        <Image
-                            className={cssModule["icon"]}
-                            src={`/itemTypes/${imageName}.png`}
-                            width={28}
-                            height={28}
-                            unoptimized
-                            alt="casque/helmet"
                         />
                     </div>
                 ))}
