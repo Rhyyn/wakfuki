04a4dac92c8bbbd98c2d375df99d28b650e106bf
Tony Migeon <tonymigeonpro@gmail.com>
2024-01-26 19:08:16 +0100

    Current version of modal

--- src/components/ModalComponents/Modal/Modal.jsx
+++ src/components/ModalComponents/Modal/Modal.jsx
@@ -1,18 +1,71 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef, useState, useCallback } from "react";
+import { useModal } from "./ModalContext";
 import cssModule from "./Modal.module.scss";
 
-const Modal = (displayedTextRef) => {
-  console.log(displayedTextRef.displayedTextRef.current);
-  // useEffect(() => {
+const Modal = () => {
+  const { modals, closeModal } = useModal();
+  const initialDurationRef = useRef();
+  const animationFrameRef = useRef();
+  const intervalRef = useRef();
+  const remainingTimeRef = useRef();
+  const progressBarRef = useRef();
 
-  // }, [displayedTextRef.displayedTextRef.current]);
+  const updateProgress = (startTime) => {
+    
+    intervalRef.current = setInterval(() => {
+      const currentTime = Date.now();
+      const elapsedTime = currentTime - startTime;
+      const remainingTime = initialDurationRef.current - elapsedTime;
+      remainingTimeRef.current = remainingTime
+      if (remainingTimeRef.current > 0) {
+        const percent = Math.round((remainingTime / initialDurationRef.current) * 100);
+        console.log(percent);
+        progressBarRef.current.style.width = `${percent}%`
+      } else {
+        clearInterval(intervalRef.current);
+        cancelAnimationFrame(animationFrameRef.current);
+      }
+    }, 80);
+
+    const update = () => {
+      animationFrameRef.current = requestAnimationFrame(update);
+    };
+
+    update();
+  };
+
+  useEffect(() => {
+    if (modals.length > 0) {
+      const { id, duration } = modals[0];
+      initialDurationRef.current = duration;
+      const startTime = Date.now();
+      updateProgress(startTime);
+
+      setTimeout(() => {
+        closeModal(id);
+      }, duration);
+
+      return () => {
+        cancelAnimationFrame(animationFrameRef.current);
+      };
+    }
+  }, [modals]);
 
   return (
-    <div className={cssModule["modal-container"]}>
-      <span className={cssModule["modal-text"]}>
-        {/* {displayedTextRef.displayedTextRef.current} */}
-      </span>
-    </div>
+    <React.Fragment>
+      {modals.map((modal) => (
+        <div className={cssModule["modal-container"]} key={modal.id}>
+          <div className={cssModule["modal-text"]}>{modal.content}</div>
+          <div className={cssModule["duration-slider"]}>
+            <div
+              ref={progressBarRef}
+              className={cssModule["slider-fill"]}
+            ></div>
+          </div>
+          {/* <button onClick={() => closeModal(modal.id)}>Close Modal</button> */}
+        </div>
+      ))}
+    </React.Fragment>
   );
 };
 
