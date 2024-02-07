c696dbaedf5cb851884ff6c7c26aec4a0ea10e02
Rhyn <tonymigeonpro@gmail.com>
2024-02-07 22:34:07 +0100

    Renamed filterState to better reflect its use -> globalFilterState

--- src/components/Contexts/GlobalContext.js
+++ src/components/Contexts/GlobalContext.js
@@ -1,6 +1,6 @@
 import { createContext, useContext, useReducer } from "react";
 
-const filterfilterState = {
+const globalglobalFilterState = {
   searchQuery: "",
   rarity: [],
   levelRange: { from: 0, to: 230 },
@@ -12,10 +12,13 @@ const filterfilterState = {
 const GlobalContext = createContext();
 
 export const GlobalContextProvider = ({ children }) => {
-  const [filterState, dispatch] = useReducer(globalReducer, filterfilterState);
-  // console.log("GlobalContext", filterState);
+  const [globalFilterState, dispatch] = useReducer(
+    globalReducer,
+    globalglobalFilterState
+  );
+  // console.log("GlobalContext", globalFilterState);
   return (
-    <GlobalContext.Provider value={{ filterState, dispatch }}>
+    <GlobalContext.Provider value={{ globalFilterState, dispatch }}>
       {children}
     </GlobalContext.Provider>
   );
@@ -29,49 +32,48 @@ export const useGlobalContext = () => {
   return context;
 };
 
-
-const globalReducer = (filterState, action) => {
+const globalReducer = (globalFilterState, action) => {
   switch (action.type) {
-    case "UPDATE_GLOBAL_filterState":
-      return { ...filterState, filterfilterState: action.payload };
+    case "UPDATE_GLOBAL_globalFilterState":
+      return { ...globalFilterState, globalglobalFilterState: action.payload };
     case "UPDATE_SEARCH_QUERY":
-      if (typeof(action.payload) === "string") {
+      if (typeof action.payload === "string") {
         console.log("setting search query: ", action.payload);
-        return { ...filterState, searchQuery: action.payload };
+        return { ...globalFilterState, searchQuery: action.payload };
       }
       console.log("Error while trying to set search query", action.payload);
-      return filterState;
+      return globalFilterState;
     case "UPDATE_RARITY":
-      if (typeof(action.payload) == "object") {
+      if (typeof action.payload == "object") {
         console.log("setting rarity: ", action.payload);
-        return { ...filterState, rarity: action.payload };
+        return { ...globalFilterState, rarity: action.payload };
       }
       console.log("Error while trying to set rarity", action.payload);
-      return filterState;
+      return globalFilterState;
     case "UPDATE_LEVEL_RANGE":
-      if (typeof(action.payload) == "object") {
+      if (typeof action.payload == "object") {
         console.log("setting level_range: ", action.payload);
-        return { ...filterState, levelRange: action.payload };
+        return { ...globalFilterState, levelRange: action.payload };
       }
       console.log("Error while trying to set level_range", action.payload);
-      return filterState;
+      return globalFilterState;
     case "UPDATE_TYPE":
-      if (typeof(action.payload) == "object") {
+      if (typeof action.payload == "object") {
         console.log("setting type: ", action.payload);
-        return { ...filterState, type: action.payload };
+        return { ...globalFilterState, type: action.payload };
       }
       console.log("Error while trying to set type", action.payload);
-      return filterState;
+      return globalFilterState;
     case "UPDATE_STATS":
-      if (typeof(action.payload) == "object") {
+      if (typeof action.payload == "object") {
         console.log("setting stats: ", action.payload);
-        return { ...filterState, stats: action.payload };
+        return { ...globalFilterState, stats: action.payload };
       }
       console.log("Error while trying to set stats", action.payload);
-      return filterState;
+      return globalFilterState;
     case "UPDATE_SORT_BY":
-      return { ...filterState, sortBy: action.payload };
+      return { ...globalFilterState, sortBy: action.payload };
     default:
-      return filterState;
+      return globalFilterState;
   }
 };
